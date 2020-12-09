5bd7040b85418cbbef9467017d41346ca04cf4e7
youCanCallMeStevie <72197493+youCanCallMeStevie@users.noreply.github.com>
2020-12-09 07:58:42 +0000

    posting needs attention

--- src/services/index.js
+++ src/services/index.js
@@ -1,82 +1,73 @@
 
-
+//bring all of your reusable consts out & up for cleanliness
 const express = require("express");
 const router = express.Router();
-// router.get("/") router.post("/:id") can create the whole collection with this router & can organise all of our "things" with this router
-const fs = require("fs"); //core module
+const fs = require("fs"); //core module, file services
 const path = require("path"); //core module
 const uniqid = require("uniqid"); //third party module, it has to be installed. this will produce unique ids for us
+const studentsFilePath = path.join(__dirname, "students.json");
+const fileAsString = fs.readFileSync(studentsFilePath).toString();
+const studentsArray = JSON.parse(fileAsString);
+
+//functions built that can be reusable too
 
 
-//1. router.get("/")
+//1.
 router.get("/", (req, res) => {
-  const studentsFilePath = path.join(__dirname, "students.json");
-  const fileAsABuffer = fs.readFileSync(studentsFilePath);
-  const fileAsAString = fileAsABuffer.toString();
-  const studentsArray = JSON.parse(fileAsAString);
-  res.send(studentsArray);
+  res.status(200).send(studentsArray);
 });
 
 //2.
 router.get("/:id", (req, res) => {
-  const studentsFilePath = path.join(__dirname, "students.json");
-  const fileAsABuffer = fs.readFileSync(studentsFilePath);
-  const fileAsAString = fileAsABuffer.toString();
-  const studentsArray = JSON.parse(fileAsAString);
-  const idComingFromRequest = req.params.id;
   const student = studentsArray.filter(
-    student => student.ID === idComingFromRequest
+    student => student.ID === req.params.id
   );
 
-  res.send(student);
+  res.status(200).send(student);
 });
 
 //3.
 router.post("/", (req, res) => {
-  const studentsFilePath = path.join(__dirname, "students.json");
-  const fileAsABuffer = fs.readFileSync(studentsFilePath);
-  const fileAsAString = fileAsABuffer.toString();
-  const studentsArray = JSON.parse(fileAsAString);
+  const newStudent = {
+    name: req.body.name,
+    surname: req.body.surname,
+    id: uniqid(),
+    email: req.body.email,
+    dob: req.body.dob,
+  };
 
-  const newStudent = req.body;
-  newStudent.ID = uniqid();
-  studentsArray.push(newStudent);
-  fs.writeFileSync(studentsFilePath, JSON.stringify(studentsArray));
-  res.status(201).send({ id: newStudent.ID });
+studentsArray.push(newStudent);
+fs.writeFileSync(studentsFilePath, JSON.stringify(studentsArray));
+res.status(201).send(newStudent);
 });
+
 
 //4.  modify a single user
 router.put("/:id", (req, res) => {
-  const studentsFilePath = path.join(__dirname, "students.json");
-  const fileAsABuffer = fs.readFileSync(studentsFilePath);
-  const fileAsAString = fileAsABuffer.toString();
-  const studentsArray = JSON.parse(fileAsAString);
+
   const newStudentsArray = studentsArray.filter(
-    student => student.ID !== req.params.id
+    student => student.id !== req.params.id
   );
 
   const modifiedStudent = req.body;
-  modifiedStudent.ID = req.params.id;
-  newStudentsArray.push(modifiedStudent);
-
-  fs.writeFileSync(usersFilePath, JSON.stringify(usersArray));
-
-  res.send(usersArray);
+  if (modifiedStudent.id === req.params.id){
+    newStudentsArray.push(modifiedStudent);
+    fs.writeFileSync(usersFilePath, JSON.stringify(usersArray));
+    res.send(usersArray);
+  } else {
+    res.status(400).send({ error: "No student found" });
+  }
 });
 
 //5.  delete a single user
 router.delete("/:id", (req, res) => {
-  const studentsFilePath = path.join(__dirname, "students.json");
-  const fileAsABuffer = fs.readFileSync(studentsFilePath);
-  const fileAsAString = fileAsABuffer.toString();
-  const studentsArray = JSON.parse(fileAsAString);
   const newStudentsArray = studentsArray.filter(
-    student => student.ID !== req.params.id
+    student => student.id !== req.params.id
   );
 
   fs.writeFileSync(studentsFilePath, JSON.stringify(newStudentsArray));
 
-  res.status(204).send();
+  res.status(200).send(newStudentsArray);
 });
 
 module.exports = router;
